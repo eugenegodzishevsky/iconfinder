8de0a4981ce1be4b35fcf9d0030cfcb794b040f9
eugenegodzishevsky <109996712+eugenegodzishevsky@users.noreply.github.com>
2024-07-31 16:29:34 +1000

    initial comit

---
+++ iconfinder/iconfinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iconfinder
+//
+//  Created by Vermut xxx on 31.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
